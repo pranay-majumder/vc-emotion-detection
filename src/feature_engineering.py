eab9c1a5c2f0475fdd263110998eca79fb4349d9
pranay-majumder <pranay.majumder17@gmail.com>
2026-07-11 03:26:09 +0530

    add TF-IDF in feature engineering step

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 import os
 import yaml
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 # Read the max_features parameter from params.yaml
 max_features = yaml.safe_load(open("params.yaml", 'r'))['feature_engineering']['max_features']
@@ -18,15 +18,15 @@ test_data = pd.read_csv('./data/processed/test_processed.csv')
 train_data = train_data.dropna(subset=['content'])
 test_data = test_data.dropna(subset=['content'])
 
-# apply BoW
+# apply TF-IDF
 X_train = train_data['content'].values
 y_train = train_data['sentiment'].values
 
 X_test = test_data['content'].values
 y_test = test_data['sentiment'].values
 
-# Apply Bag of Words (CountVectorizer)
-vectorizer = CountVectorizer(max_features=max_features)
+# Apply TF-IDF
+vectorizer = TfidfVectorizer(max_features=max_features)
 
 # Fit the vectorizer on the training data and transform it
 X_train_bow = vectorizer.fit_transform(X_train)
@@ -47,6 +47,6 @@ data_path = os.path.join("data","features")
 
 os.makedirs(data_path)
 
-train_df.to_csv(os.path.join(data_path,"train_bow.csv"))
-test_df.to_csv(os.path.join(data_path,"test_bow.csv"))
+train_df.to_csv(os.path.join(data_path,"train_tfidf.csv"))
+test_df.to_csv(os.path.join(data_path,"test_tfidf.csv"))
 
--- src/model_building.py
+++ src/model_building.py
@@ -12,7 +12,7 @@ n_estimators = yaml.safe_load(open("params.yaml", 'r'))['model_building']['n_est
 learning_rate = yaml.safe_load(open("params.yaml", 'r'))['model_building']['learning_rate']
 
 # fetch the data from data/processed
-train_data = pd.read_csv('./data/features/train_bow.csv')
+train_data = pd.read_csv('./data/features/train_tfidf.csv')
 
 X_train = train_data.iloc[:,0:-1].values
 y_train = train_data.iloc[:,-1].values
